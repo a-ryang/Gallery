9b4b73744fa2743fb6cb3d5c3c34d363eab61f92
a-ryang <chnwu.kim@gmail.com>
2022-11-22 22:35:52 +0900

    feat: item display

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,18 +1,5 @@
 <template>
   <div class="home">
-    <section class="py-5 text-center container">
-      <div class="row py-lg-5">
-        <div class="col-lg-6 col-md-8 mx-auto">
-          <h1 class="fw-light">Album example</h1>
-          <p class="lead text-muted">item</p>
-          <p>
-            <a href="#" class="btn btn-primary my-2">Main call to action</a>
-            <a href="#" class="btn btn-secondary my-2">Secondary action</a>
-          </p>
-        </div>
-      </div>
-    </section>
-
     <div class="album py-5 bg-light">
       <div class="container">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
@@ -48,4 +35,15 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.card .img {
+  display: inline-block;
+  width: 100%;
+  height: 250px;
+  background-size: cover;
+}
+
+.card .card-body .price {
+  text-decoration: line-through;
+}
+</style>
